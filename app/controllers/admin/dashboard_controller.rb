fea959daa1f5bd8ce5cf4d4ad1f1b4dba292671e
hayato-iwata <wakuwakudog.mentor@gmail.com>
2026-03-22 15:19:07 +0900

    Top画面を作成、企業画面と分離

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,5 @@
+module Admin
+  class DashboardController < Admin::ApplicationController
+    def home; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :companies
-    root to: "companies#index"
+    root to: "dashboard#home"
   end
 
   resources :companies
