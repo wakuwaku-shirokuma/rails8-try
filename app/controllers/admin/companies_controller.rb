fa11480e5be9285e4c8bcf15847f5a182e689460
hayato-iwata <wakuwakudog.mentor@gmail.com>
2026-03-22 15:25:53 +0900

    app/controllers/admin/companies_controller.rb のリファクタリング

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,5 +1,7 @@
 module Admin
   class CompaniesController < Admin::ApplicationController
+    before_action :set_resource, only: %i[edit show update]
+
     def scoped_resource
       resource_class.order(:id)
     end
@@ -22,34 +24,17 @@ module Admin
     end
 
     def edit
-      resource = requested_resource
-      authorize_resource(resource)
+      authorize_resource(@resource)
 
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(
-            view_context.dom_id(resource),
-            partial: "admin/companies/form_row",
-            locals: { resource: resource }
-          )
-        end
-
+        format.turbo_stream { render turbo_stream: form_row_stream(@resource) }
         format.html
       end
     end
 
     def show
-      resource = requested_resource
-
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(
-            view_context.dom_id(resource),
-            partial: "admin/companies/row",
-            locals: { resource: resource }
-          )
-        end
-
+        format.turbo_stream { render turbo_stream: row_stream(@resource) }
         format.html
       end
     end
@@ -60,49 +45,37 @@ module Admin
 
       if resource.save
         respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: [
-              turbo_stream.replace(
-                "new_company",
-                partial: "admin/companies/row",
-                locals: { resource: resource }
-              )
-            ]
-          end
-
+          format.turbo_stream { render turbo_stream: row_stream(resource, id: "new_company") }
           format.html { redirect_to admin_companies_path }
         end
       else
-        render turbo_stream: turbo_stream.replace(
-          "new_company",
-          partial: "admin/companies/form_row",
-          locals: { resource: resource }
-        ), status: :unprocessable_entity
+        render turbo_stream: form_row_stream(resource, id: "new_company"), status: :unprocessable_entity
       end
     end
 
     def update
-      resource = requested_resource
-
-      if resource.update(resource_params)
+      if @resource.update(resource_params)
         respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: turbo_stream.replace(
-              view_context.dom_id(resource),
-              partial: "admin/companies/row",
-              locals: { resource: resource }
-            )
-          end
-
+          format.turbo_stream { render turbo_stream: row_stream(@resource) }
           format.html { redirect_to admin_companies_path }
         end
       else
-        render turbo_stream: turbo_stream.replace(
-          view_context.dom_id(resource),
-          partial: "admin/companies/form_row",
-          locals: { resource: resource }
-        ), status: :unprocessable_entity
+        render turbo_stream: form_row_stream(@resource), status: :unprocessable_entity
       end
+    end
+
+    private
+
+    def set_resource
+      @resource = requested_resource
+    end
+
+    def row_stream(resource, id: view_context.dom_id(resource))
+      turbo_stream.replace(id, partial: "admin/companies/row", locals: { resource: resource })
+    end
+
+    def form_row_stream(resource, id: view_context.dom_id(resource))
+      turbo_stream.replace(id, partial: "admin/companies/form_row", locals: { resource: resource })
     end
   end
 end
